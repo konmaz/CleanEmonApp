4904870d25c2047fa15f920df3592722de6c0ec8
Konstantinos Mazgaltzidis <kamazgal@csd.auth.gr>
2023-06-02 00:13:55 +0300

    Implement feature to display disaggregated consumption of each appliance within a specified date range

--- code/charts.js
+++ code/charts.js
@@ -2,6 +2,11 @@ import {API} from './config.js';
 
 const DEVICE = localStorage.getItem('emon_id');
 
+const SPINER = `<div class="spinner-border text-primary mt-5" role="status" style="width:5rem; height: 5rem" >
+  <span class="visually-hidden">Loading...</span>
+</div>`
+
+
 function single_day_consumption(date) {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', `${API}/dev_id/${DEVICE}/json/date/${date}?downsampling=true`, true);
@@ -9,9 +14,7 @@ function single_day_consumption(date) {
 
     xhr.onloadstart = function () {
         const loadingText = document.getElementById('image-placeholder');
-        loadingText.innerHTML = `<div class="spinner-border text-primary mt-5" role="status" style="width:5rem; height: 5rem" >
-  <span class="visually-hidden">Loading...</span>
-</div>`;
+        loadingText.innerHTML = SPINER;
     };
 
     xhr.onload = function () {
@@ -64,9 +67,7 @@ function fetch_consumptions(number_of_days) {
 
     xhr.onloadstart = function () {
         const loadingText = document.getElementById('image-placeholder_consumption_history');
-        loadingText.innerHTML = `<div class="spinner-border text-primary mt-5" role="status" style="width:5rem; height: 5rem" >
-  <span class="visually-hidden">Loading...</span>
-</div>`;
+        loadingText.innerHTML = SPINER;
     };
 
     xhr.onload = function () {
@@ -244,15 +245,206 @@ buttonGroup.addEventListener('click', function (event) {
 });
 
 
+
 fetch_consumptions(7);
+function dateToISO(date){
+    let currentDay= String(date.getDate()).padStart(2, '0');
+    let currentMonth = String(date.getMonth()+1).padStart(2,"0");
+    let currentYear = date.getFullYear();
+    let currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+
+    return currentDate;
+}
+
 
 const date = new Date();
-let currentDay= String(date.getDate()).padStart(2, '0');
-let currentMonth = String(date.getMonth()+1).padStart(2,"0");
-let currentYear = date.getFullYear();
-let currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+
 
 
 var datepicker = document.getElementById('date_picker');
-datepicker.value = currentDate;
+datepicker.value = dateToISO(date);
 datepicker.dispatchEvent(new Event('change'));
+
+const div_list_appliances_consumptions_list = document.getElementById('Appliances_Consumptions_list');
+const button_query_appliances_consumptions = document.getElementById('button_query_appliances_consumptions');
+const appliances_consumption_start = document.getElementById('appliances_date_picker_start');
+const appliances_consumption_end = document.getElementById('appliances_date_picker_end');
+
+
+function appliances_consumptions_list(start,end) {
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `${API}/dev_id/${DEVICE}/pred_consumption/range/${start}/${end}?summarize=true`, true);
+    xhr.setRequestHeader('Authorization', `Bearer ${localStorage.getItem('bearer_token')}`);
+
+    xhr.onloadstart = function () {
+        div_list_appliances_consumptions_list.innerHTML = SPINER;
+    };
+
+    xhr.onload = function () {
+
+        if (this.status === 200) {
+            // console.log(JSON.parse(xhr.responseText).energy_data);
+            div_list_appliances_consumptions_list.innerHTML = ""
+            let data;
+            data = JSON.parse(xhr.responseText)
+            console.log(data)
+
+            for(const key in data){
+                let key_formatted = key.replaceAll("_", " ").replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());;
+                if (key.startsWith('pred'))
+                    key_formatted = key_formatted.substring(4);
+
+                div_list_appliances_consumptions_list.insertAdjacentHTML('beforeend',
+                    `<div class="row justify-content-center">
+            <div class="col-5">
+                <p class="fs-3">${key_formatted}</p>
+            </div>
+            <div class="col-5">
+                <p class="fs-3 green text-nowrap bg-body-tertiary rounded">${Number(data[key]).toFixed(2) + ' kWh'}</p>
+            </div>
+        </div>`)
+            }
+        }
+        else if (this.status === 400){
+            div_list_appliances_consumptions_list.innerHTML = JSON.parse(xhr.responseText).message;
+        }
+    }
+
+    xhr.send();
+}
+
+function appliances_consumptions_graph(start,end) {
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `${API}/dev_id/${DEVICE}/pred_consumption/range/${start}/${end}?summarize=false`, true);
+    xhr.setRequestHeader('Authorization', `Bearer ${localStorage.getItem('bearer_token')}`);
+
+    xhr.onloadstart = function () {
+    };
+
+    xhr.onload = function () {
+
+        if (this.status === 200) {
+            // console.log(JSON.parse(xhr.responseText).energy_data);
+            let data;
+            data = JSON.parse(xhr.responseText)
+            console.log(data)
+
+            const labels = data.map(item => {
+                let key = item.id;
+                let key_formatted = key.replaceAll("_", " ").replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+                return key_formatted;
+            });
+
+            const keys = Object.keys(data[0].value);
+            const datasets = keys.map((key, index) => {
+                let label = key;
+                let key_formatted = label.replaceAll("_", " ").replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+                if (key.startsWith('pred'))
+                    key_formatted = key_formatted.substring(4);
+
+                return {
+                    label: key_formatted,
+                    data: data.map(item => item.value[key]),
+                    borderWidth: 1,
+                    order: index,
+                    hidden: key_formatted === 'Noise'
+                };
+            });
+
+            show_appliances_consumptions_graph(labels, datasets);
+
+        }
+    }
+
+    xhr.send();
+}
+let appliancesChart;
+appliancesChart = null;
+function show_appliances_consumptions_graph(labels, datasets){
+    if (appliancesChart !== null)
+        appliancesChart.destroy();
+
+    const datesFormatted = labels.map(function (entry) {
+        const date = new Date(entry);
+        const day = date.getDate();
+        const month = date.toLocaleString('default', { month: 'short' });
+        return `${day} ${month}`;
+    });
+
+    var ctx = document.getElementById('appliancesChart').getContext('2d');
+    appliancesChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: datesFormatted,
+            datasets: datasets
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            aspectRatio: 1,
+            plugins: {
+                legend: {
+                    position: 'top',
+                    labels: {
+                        // Adjust the font size of the legend items
+                        font: {
+                            size: 16
+                        },padding: 10
+                    }
+                }
+            }
+            ,
+            indexAxis: 'x',
+            scales: {
+                x: {
+                    display: true,
+                    title: {
+                        display: true,
+                        text: 'Date'
+                    }
+                }
+            }
+        }
+    })
+
+}
+
+button_query_appliances_consumptions.addEventListener('click', function (event) {
+    //div_list_appliances_consumptions_list.innerHTML = SPINER;
+
+    appliances_consumptions_graph(appliances_consumption_start.value, appliances_consumption_end.value);
+    appliances_consumptions_list(appliances_consumption_start.value, appliances_consumption_end.value);
+
+
+})
+
+const startDateInput = document.getElementById('appliances_date_picker_start');
+const endDateInput = document.getElementById('appliances_date_picker_end');
+
+const today = new Date();
+
+// Subtract one day to get yesterday's date
+const yesterday = new Date(today);
+yesterday.setDate(today.getDate() - 1);
+
+
+// Set max date for start date input
+endDateInput.setAttribute('max', dateToISO(yesterday));
+
+startDateInput.setAttribute('max', dateToISO(yesterday));
+
+// Set min date for end date input
+startDateInput.addEventListener('change', function () {
+    endDateInput.setAttribute('min', startDateInput.value);
+});
+
+// Set min date for start date input
+endDateInput.addEventListener('change', function () {
+    startDateInput.setAttribute('max', endDateInput.value);
+});
+
+
+
+
